77d6fe03f592c0ef72768e8d683d7cb9bafee965
Pulkit Bhutani <pulkitbhutani@outlook.com>
2021-01-04 17:00:39 +0530

    basic ui and api call to fetch

---
+++ scripts.js
@@ -0,0 +1,52 @@
+const form = document.querySelector('.search-form');
+
+form.addEventListener('submit', function(f){
+
+    //remove old items from the list
+
+    const song = document.getElementById('song').value;
+    f.preventDefault();
+    console.log(song);
+    fetchSuggestions(song);
+})
+
+function fetchSuggestions(song)
+{
+
+    const suggestUrl = 'https://api.lyrics.ovh/suggest/'+song;
+    console.log(suggestUrl); 
+
+    fetch(suggestUrl)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data.data);
+        populateList(data.data);
+    })
+    .catch(function(error) {
+        console.log(error);
+      });
+
+    function populateList(data){
+        const songList = document.getElementById('songList');
+        for (song of data){
+            console.log(song);
+            let li = document.createElement('li');
+            li.className = 'song-li';
+            let node = document.createTextNode(song.artist.name + ' - ' + song.title);
+            let button = document.createElement('button');
+            button.innerHTML='Show Lyrics';
+            button.click(song.artist.name);
+            li.appendChild(node);
+            li.appendChild(button);
+            songList.appendChild(li);
+        }
+    }
+        
+}
+
+function showLyrics()
+{
+    console.log(song.artist.name);
+}
